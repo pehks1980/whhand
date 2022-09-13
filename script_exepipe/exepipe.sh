8e12c6da613fc7a22f38048729642d59c3fe8dcd
ass <holen@bitcoin.com>
2022-09-13 15:20:23 +1000

    exepipe.sh corrected

--- script_exepipe/exepipe.sh
+++ script_exepipe/exepipe.sh
@@ -5,13 +5,16 @@ home_dir=/home/user/ansible
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 
-echo "$dt started." >> $home_dir/exepipe.log
+#touch -a $home_dir/exepip.log
+
+echo "$dt exepipe started." >> $home_dir/exepipe.log
 
 while true 
 do
-     dt=$(date '+%d/%m/%Y %H:%M:%S');
 
      cmd=$(cat $home_dir/my_exe_pipe);
+
+     dt=$(date '+%d/%m/%Y %H:%M:%S');
 
      echo "$dt received command: $cmd " >> $home_dir/exepipe.log	
      
