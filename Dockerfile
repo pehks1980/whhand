65683688478d5e7a19f80b479e3c5006e12ed080
Pehks <pehks1980@gmail.com>
2025-02-27 12:28:56 +0300

    Added env variables (WHPORT,WHSHELLCMD,WHSECRET) support, it can be set up now

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM golang:1.18 as modules
+FROM golang:1.23 AS modules
 
 ADD go.mod go.sum /m/
 RUN cd /m && go mod download
-
-FROM golang:1.18 as builder
+#cpy folder from modules to builder
+FROM golang:1.23 AS builder
 COPY --from=modules /go/pkg /go/pkg
+#mkdir if its doesnt exist yet
 RUN mkdir -p /whhand
 
 ADD . /whhand
@@ -15,12 +16,12 @@ RUN useradd -u 1001 user
 # Собираем бинарный файл GOARCH=arm64 pi | amd64 x86
 # main - это результирующий exe файл в корне билдера
 RUN GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o /main
-
+#chown exe to be owned by user
 RUN chown user /main
 
-
+#final dest layer image
 FROM alpine
-
+#copy user passwd info from builder
 COPY --from=builder /etc/passwd /etc/passwd
 # because program creates file inside we need to use image alpine which has commands to
 # we make special dir with user rights
@@ -32,7 +33,7 @@ WORKDIR /whhand
 
 # exe file has to have its own name different from dir!
 COPY --from=builder /main /whhand/main
-
+# starting pathfile of the container
 CMD ["/whhand/main"]
 
 # docker build -t whhand .
@@ -46,7 +47,7 @@ CMD ["/whhand/main"]
 # save it in tar archive
 # docker save whhand:arm64  > whhand-arm64.tar
 # copy to pi via scp
-# scp whhand-arm64.tar user@192.168.1.204:/home/user
+# scp whhand-arm64.tar user@192.168.31.204:/home/user
 # unpack tar to local image docker repo (on pi side)
 # docker load -i whhand-arm64.tar
 # check & run
@@ -55,3 +56,7 @@ CMD ["/whhand/main"]
 # docker run -p 8989:8989 -v /home/user/ansible:/export -d whhand:arm64
 # script has to be run at reboot time or manually - &
 # @reboot /home/user/ansible/exepipe.sh
+# save image to dockerhub
+# docker login -u pehks1980 (pswd)
+# docker tag  whhand:arm64 pehks1980/repo2:whhand_1
+# docker push pehks1980/repo2:whhand_1
--- main.go
+++ main.go
@@ -32,40 +32,80 @@ import (
 
 // App - application contents & methods
 type App struct {
-	CTX context.Context
+	CTX    context.Context
 	cmdstr string
+	secret string
 }
 
 const ShellToUse = "/bin/sh" //"bash"
 // webhook secret
-const secret = "my-secret-key-1980!A"
+const secret1 = "my-secret-key-1980!A"
+// port to listen
+const whport = "8989"
+
 // mode standalone app cmd to launch ansible
-const shellcmd = "ansible-playbook -i '192.168.1.204, ' /home/user/ansible/play_depl.yml"
+const shellcmd2 = "ansible-playbook -i '192.168.31.204, ' /home/user/ansible/play_depl.yml"
+
 // docker container mode cmd to launch ansible should be sent to fifo pipe which should exist on mapped folder on host
 // (inner is 'export' for this case)
-const shellcmd1 = "echo \"ansible-playbook -i '192.168.1.204, ' /home/user/ansible/play_depl.yml\" > /export/my_exe_pipe"
+const shellcmd1 = "echo \"ansible-playbook -i '192.168.31.204, ' /home/user/ansible/play_depl.yml\" > /export/my_exe_pipe"
 
-func Shellout(command string) (error, string, string) {
+func Shellout(command string) (string, string, error) {
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	cmd := exec.Command(ShellToUse, "-c", command)
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 	err := cmd.Run()
-	return err, stdout.String(), stderr.String()
+	return stdout.String(), stderr.String(), err
 }
 
 func main() {
 
-	app := App{
-		CTX: context.Background(),
-		cmdstr: shellcmd1, //docker mode
+	portDef := flag.String("webhook incoming port", whport, "WHPORT")
+
+	port := os.Getenv("WHPORT")
+	if port == "" {
+		log.Printf("$WHPORT is not set. Using default: %s", *portDef)
+		port = *portDef
+	} else {
+		log.Printf("Using env $WHPORT = %s", port)
 	}
-	log.Print("Starting the app for github webhook")
-	port := flag.String("port", "8989", "Port")
+
+	secretDef := flag.String("webhook incoming secret", secret1, "WHSECRET")
+
+	secret := os.Getenv("WHSECRET")
+	if secret == "" {
+		log.Printf("$WHSECRET is not set. Using default: ******")
+		secret = *secretDef
+	} else {
+		log.Printf("Using env $WHSECRET = %s", secret)
+	}
+
 	shutdownTimeout := flag.Int64("shutdown_timeout", 3, "shutdown timeout")
+    
+	log.Print("Starting the app for github webhook")
+
+	shellcmdDef := flag.String("webhook command", shellcmd1, "It can be also set as WHSHELLCMD.")
+
+    shellcmd := os.Getenv("WHSHELLCMD")
+	if shellcmd == "" {
+		log.Printf("$WHSHELLCMD is not set. Using default (for Docker): %s", *shellcmdDef)
+		shellcmd = *shellcmdDef
+	} else {
+		log.Printf("Using env $WHSHELLCMD = %s", shellcmd)
+	}
+
+	app := App{
+		CTX:    context.Background(),
+		cmdstr: shellcmd, //docker mode/standalone up to shell
+		secret: secret,
+	}
+
+	log.Printf("cmd: %s", app.cmdstr)
+
 	serv := http.Server{
-		Addr:    net.JoinHostPort("", *port),
+		Addr:    net.JoinHostPort("", port),
 		Handler: app.RegisterPublicHTTP(),
 	}
 	// запуск сервера
@@ -78,7 +118,7 @@ func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 
-	log.Printf("Started app at :%s", *port)
+	log.Printf("Started app at :%s", port)
 	// ждет сигнала
 	sig := <-interrupt
 
@@ -98,7 +138,6 @@ func (app *App) RegisterPublicHTTP() *mux.Router {
 	r.HandleFunc("/webhook", app.hookHandlerPost()).Methods(http.MethodPost)
 	// diag
 	r.HandleFunc("/webhook", app.hookHandlerGet()).Methods(http.MethodGet)
-	// user crud
 	return r
 }
 
@@ -106,13 +145,13 @@ func (app *App) RegisterPublicHTTP() *mux.Router {
 func (app *App) hookHandlerPost() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		/*
-		payload, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("error reading request body: err=%s\n", err)
-			return
-		}
-*/
-		payload, err := github.ValidatePayload(r, []byte(secret))
+			payload, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				log.Printf("error reading request body: err=%s\n", err)
+				return
+			}
+		*/
+		payload, err := github.ValidatePayload(r, []byte(app.secret))
 		if err != nil {
 			log.Printf("error validating request body: err=%s\n", err)
 			return
@@ -144,7 +183,7 @@ func (app *App) hookHandlerPost() func(http.ResponseWriter, *http.Request) {
 			if branch == "master" {
 				go func() {
 					log.Printf("master branch push event, sha: %s", *e.After)
-					err, out, errout := Shellout(app.cmdstr)
+					out, errout, err := Shellout(app.cmdstr)
 					if err != nil {
 						log.Printf("error: %v\n", err)
 					}
@@ -165,8 +204,8 @@ func (app *App) hookHandlerGet() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 
-		line := "from: " + r.RemoteAddr +" to: "+ r.Host  + r.URL.String()
-		log.Printf("diag test is OK - received message: %s\n",line)
+		line := "from: " + r.RemoteAddr + " to: " + r.Host + r.URL.String()
+		log.Printf("diag test is OK - received message: %s\n", line)
 		writeResponse(w, http.StatusOK, strings.ToUpper(line))
 	}
 }
